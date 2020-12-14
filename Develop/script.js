678835b19a5d39112fa176c5b159c0a09f00b93e
aqrit <aqrit@aqr.local>
2020-12-14 14:14:34 +1100

    Save Event Function and Local Storage

--- Develop/script.js
+++ Develop/script.js
@@ -7,4 +7,32 @@ $(document).ready(function () {
         return currentDay;
     }
     displayDate()
+
+    let task = []
+    // retrieve any pre-exsisting tasks from local storage
+    if (localStorage.getItem("items") !== null) {
+        task = JSON.parse(localStorage.getItem("items"));
+        for (i = 0; i < task.length; i++) {
+             var hour = Object.keys(task[i])[0];
+             var text = task[i][hour];
+             $(".description").each(function() {
+                if ($(this).attr("id") === hour) {
+                    $(this).val(text);
+                }
+             })
+         
+        } 
+     }
+    //save button event listener to save any tasks into the local storage 
+    $(".saveBtn").click(
+        function() {
+            var text = $(this).siblings("textarea").val().trim();
+            var hourlyEntry = $(this).siblings("textarea").attr("id");
+            var object = {};
+            object[hourlyEntry] = text;
+            task.push(object);
+            localStorage.setItem("items", JSON.stringify(task));
+
+         }
+    )
 });
